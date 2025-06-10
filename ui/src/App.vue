bf0c80b1bb816dfa0cb936eaa550cdc8ec17c1e2
Ryan Peters <ryan.peters@impact.com>
2025-06-09 20:27:18 -0700

    small css change to fix side nav sort of

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -67,6 +67,9 @@ document.documentElement.setAttribute('data-theme', 'light');
   width: 15%;
   padding: 1rem;
   box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 .content {
