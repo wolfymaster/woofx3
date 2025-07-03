f511d0940218731a786725340e79425439cc75b0
wolfy <wolfy@wolfymaster.com>
2025-07-03 07:25:32 -0400

    add ui.local.woofx3.tv to ui allowedHosts

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173, // you can replace this port with any port
+    allowedHosts: ['ui.local.woofx3.tv']
   },
   build: {
     manifest: true,
